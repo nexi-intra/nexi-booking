0a8add1058736bf2f2fab368e9e660240c6b8c7b
Niels Gregers Johansen <niels@timeboxer.dk>
2024-06-18 10:33:52 +0200

    integrated and renamed

--- .koksmat/web/app/booking/page.tsx
+++ .koksmat/web/app/booking/page.tsx
@@ -3,16 +3,13 @@
 import SearchPerson from "@/app/magic/services/magic-people/person/components/search";
 import { useRouter } from "next/navigation";
 import { APPNAME } from "../global";
+import { Dashboard } from "@/components/dashboard";
 
 export default function Page() {
   const router = useRouter();
   return (
     <div className="space-x-2 h-[90vh]">
-      <SearchPerson
-        onItemClick={(item) => {
-          router.push(`/${APPNAME}/persons/${item.id}`);
-        }}
-      />
+      <Dashboard />
     </div>
   );
 }
